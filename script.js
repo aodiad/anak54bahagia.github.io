7861d7db5247c427197162d9db085ae36d804dea
aodiad <rehandanil14@gmail.com>
2024-08-10 11:17:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent form from submitting the traditional way
+
+    // You can handle form data here or send it to a server
+    alert('makasih yaa udah mau tau.');
+});
